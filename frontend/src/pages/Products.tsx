340bd85fb459e1d63c3c12624ab51a5630e0b07a
efipos <deploy@efipos.com>
2026-07-21 10:34:27 -0400

    fix unclosed div in prices section

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -426,6 +426,7 @@ export default function Products() {
                         className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
                     </div>
                   </div>
+                </div>
               </div>
 
               {/* Moneda, IVA, Stock */}
